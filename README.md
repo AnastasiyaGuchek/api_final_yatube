dbde0cbeedb65feb1a667f76bfdae7d1d8241246
Анастасия Гучек <anastasia.guchek@mail.ru>
2023-03-04 15:35:00 +0300

    code according to PEP8

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# api_final
-api final
+Описание проекта.
+В данном проекте реализована социальная сеть для обмена личной информацией и картинками.
+
+Технологии.
+Python, Django, DRF, Simple JWT.
+
+Как запустить проект.
+
+Клонировать репозиторий и перейти в его папку в командной строке:
+
+git clone https://github.com/AnastasiyaGuchek/api_final_yatube.git
+cd api_final_yatube
+Cоздать и активировать виртуальное окружение:
+
+python -m venv venv
+
+Для *nix-систем:
+source venv/bin/activate
+
+Для windows-систем:
+source venv/Scripts/activate
+
+Установить зависимости из файла requirements.txt:
+
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+Выполнить миграции:
+cd yatube_api
+python manage.py migrate
+
+Создать суперпользователя django:
+python manage.py createsuperuser
+
+Запустить проект:
+python manage.py runserver
+
+Разработчик
+Анастасия Гучек
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,62 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
-
-    class Meta:
-        fields = '__all__'
-        model = Post
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+    author = SlugRelatedField(
+        read_only=True,
+        slug_field='username'
     )
 
     class Meta:
+        model = Post
         fields = '__all__'
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
         model = Comment
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    following = SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username'
+    )
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        validators = (
+            serializers.UniqueTogetherValidator(
+                queryset=model.objects.all(),
+                fields=('user', 'following'),
+                message=("Вы уже подписаны на этого автора.")
+            ),
+        )
+
+    def validate(self, data):
+        if data['following'] == self.context['request'].user:
+            raise serializers.ValidationError(
+                'Вы не можете подписаться на самого себя.')
+        return data
